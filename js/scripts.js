9365ad86bdc72d6f9e0429de203da22e68011cdb
Kelsey Lawrence <kelseylawrence@Kelseys-MacBook-Pro.local>
2013-08-28 12:04:10 -0700

    Added grid and gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 jQuery(document).ready(function($) {
+	
+	$('.posts').masonry({
+  		// columnWidth: 200,
+  		itemSelector: '.post'
+	});
+
+	$("#slides").slidesjs({
+		navigation : {
+			effect : "fade"
+		}
+		
+	});
 
 });
 
